e4d6e8e963f75f21c4b54d54fd023a4a93d7f287
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-02-11 22:04:30 +0600

    Finish shopping_cart

--- payment/shopping_cart/api/v1/views.py
+++ payment/shopping_cart/api/v1/views.py
@@ -1,13 +1,8 @@
-import datetime
-from profile.models import Profile
-
-from config.project_config import settings
-from django.conf import settings
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
-from django.urls import reverse
+
 from item.models import Item
+from profile.models import Profile
 from shopping_cart.extras import generate_order_id
 from shopping_cart.models import Order, OrderItem
 
@@ -25,32 +20,24 @@ def add_to_cart(request, item_id):
     profile = get_object_or_404(Profile, user=request.user)
     item = get_object_or_404(Item, id=item_id)
     if item in profile.items.all():
-        messages.info(request, "You already own this item")
         return redirect("item:item_list")
 
     order_item, created = OrderItem.objects.get_or_create(item=item)
-    print(created)
-
     user_order, created = Order.objects.get_or_create(owner=profile, is_ordered=False)
-    print(created)
 
     user_order.items.add(order_item)
     if created:
         user_order.ref_code = generate_order_id()
         user_order.save()
 
-    messages.info(request, "Item added to cart")
     return redirect("item:item_list")
 
 
 @login_required()
 def delete_from_cart(request, item_id):
-    item_to_delete = OrderItem.objects.filter(pk=item_id)
-    if item_to_delete.exists():
-        item_to_delete[0].delete()
-        messages.info(request, "Item has been deleted")
-
-    return redirect(reverse("shopping_cart:order_summary"))
+    item_to_delete = get_object_or_404(OrderItem, id=item_id)
+    item_to_delete.delete()
+    return redirect("shopping_cart:order_summary")
 
 
 @login_required()
@@ -58,39 +45,3 @@ def order_details(request, **kwargs):
     existing_order = get_user_pending_order(request)
     context = {"order": existing_order}
     return render(request, "shopping_cart/order_summary.html", context)
-
-
-@login_required()
-def checkout(request):
-    existing_order = get_user_pending_order(request)
-    context = {"order": existing_order}
-    return render(request, "checkout.html", context)
-
-
-@login_required()
-def process_payment(request, order_id):
-    return redirect(
-        reverse("shopping_cart:update_records", kwargs={"order_id": order_id})
-    )
-
-
-@login_required()
-def update_transaction_records(request, order_id):
-    order_to_purchase = Order.objects.filter(pk=order_id).first()
-
-    order_to_purchase.is_ordered = True
-    order_to_purchase.date_ordered = datetime.datetime.now()
-    order_to_purchase.save()
-
-    order_items = order_to_purchase.items.all()
-
-    order_items.update(is_ordered=True, date_ordered=datetime.datetime.now())
-
-    profile = get_object_or_404(Profile, user=request.user)
-
-    order_items = [item.product for item in order_items]
-    profile.items.add(*order_items)
-    profile.save()
-
-    messages.info(request, "Your items have been added to your cart.")
-    return redirect(reverse("accounts:my_profile"))
--- payment/shopping_cart/models.py
+++ payment/shopping_cart/models.py
@@ -1,8 +1,7 @@
-from profile.models import Profile
-
-from django.contrib.auth.models import User
 from django.db import models
+
 from item.models import Item
+from profile.models import Profile
 
 
 class OrderItem(models.Model):
@@ -21,7 +20,6 @@ class OrderItem(models.Model):
 class Order(models.Model):
     ref_code = models.CharField(max_length=15)
     owner = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
-    # payment_details = models.ForeignKey(Payment, null=True)
     is_ordered = models.BooleanField(default=False)
     items = models.ManyToManyField(OrderItem)
     date_ordered = models.DateTimeField(auto_now=True)
@@ -35,20 +33,8 @@ class Order(models.Model):
     def get_cart_total(self):
         return sum([item.item.get_price() for item in self.items.all()])
 
+    def get_cart_total_cents(self):
+        return sum([item.item.price for item in self.items.all()])
+
     def __str__(self):
         return f"{self.owner.user} - {self.ref_code}"
-
-
-class Transaction(models.Model):
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    token = models.CharField(max_length=120)
-    order_id = models.CharField(max_length=120)
-    amount = models.DecimalField(max_digits=100, decimal_places=2)
-    success = models.BooleanField(default=True)
-    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
-
-    def __str__(self):
-        return self.order_id
-
-    class Meta:
-        ordering = ["-timestamp"]
